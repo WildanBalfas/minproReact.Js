293e3c18a186db727978c64b81b17a6375d3b546
Rozinul Mahrus <rozinulmahrus@gmail.com>
2018-08-10 11:43:57 +0700

    add menu & role

--- src/components/layout/sidebar.js
+++ src/components/layout/sidebar.js
@@ -6,12 +6,16 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import HomeIcon from '@material-ui/icons/Home';
 import HelpIcon from '@material-ui/icons/Help';
+import MenuIcon from '@material-ui/icons/AppsRounded';
+import RoleIcon from '@material-ui/icons/MeetingRoomRounded'
 import IconButton from '@material-ui/core/IconButton';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import Divider from '@material-ui/core/Divider';
 
 import Company from '../company';
 import Souvenir from '../souvenir';
+import Menu from '../menu';
+import Role from '../role';
 
 export default class SideBar extends React.Component {
     render() {
@@ -30,13 +34,16 @@ export default class SideBar extends React.Component {
                         <ListItem className={classes.ListItem}><HomeIcon className={classes.MenuIcon} /><Link to="/" className={classes.MenuList}>Home</Link></ListItem>
                         <ListItem className={classes.ListItem}><HomeIcon className={classes.MenuIcon} /><Link to="/company" className={classes.MenuList}>Company</Link></ListItem>
                         <ListItem className={classes.ListItem}><HomeIcon className={classes.MenuIcon} /><Link to="/souvenir" className={classes.MenuList}>Souvenir</Link></ListItem>
+                        <ListItem className={classes.ListItem}><MenuIcon className={classes.MenuIcon} color="primary" /><Link to="/menu" className={classes.MenuList}>Menu</Link></ListItem>
+                        <ListItem className={classes.ListItem}><RoleIcon className={classes.MenuIcon} color="primary" /><Link to="/role" className={classes.MenuList}>Role</Link></ListItem>
                         <ListItem className={classes.ListItem}><HelpIcon className={classes.MenuIcon} /><Link to="/help" className={classes.MenuList}>Help</Link></ListItem>
                     </List>
                 </Drawer>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/company" component={Company} />
                 <Route exact path="/souvenir" component={Souvenir} />
-                <Route exact path="/help" component={Help} />
+                <Route exact path="/menu" component={Menu} />
+                <Route exact path="/role" component={Role} />
             </div>
         )
     }
