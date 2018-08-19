aa1968c3fd9159e6696cc27e21b4eb7a2351bf5a
WildanBalfas <wildanbalfas3@gmail.com>
2018-08-19 10:43:29 +0700

    updates promo

--- src/components/promotion/create.js
+++ src/components/promotion/create.js
@@ -4,87 +4,53 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import { TextField } from '../../../node_modules/@material-ui/core';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 
-export default ({ createNew, handleToggle, handleClose, handleChange, handleSubmit, user: { username, password, re_password, mRoleId, mEmployeeId }, employees, roles }) => {
-    // shrink
+export default ({createNew,handleToggle,handleChange,handleClose,handleSubmit, promo: {t_event_id, event_code}, events}) =>{
+    
     return <Fragment>
-        <Button onClick={handleToggle} variant="contained" color="primary">Create</Button>
-        <Dialog
-            open={createNew}
-            onClose={handleClose}        >
-            <DialogTitle id="alert-dialog-title">{"Create New User"}</DialogTitle>
-            <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                    Please fill out the form below!
-                    <form>
-                        <FormControl fullWidth="true">
-                            <InputLabel shrink htmlFor="unit-simple">Role Name</InputLabel>
-                            <Select
-                                value={mRoleId}
-                                onChange={handleChange('mRoleId')}
-                                inputProps={{
-                                    name: 'mRoleId',
-                                    id: 'unit-simple',
-                                }}
-                            >
-                                <MenuItem value="">
-                                    <em>-Select Role Name-</em>
-                                    </MenuItem>
-                                {roles.map(r => {
-                                    return (
-                                        <MenuItem value={r._id}>{r.name}</MenuItem>
-                                    )
-                                })}
-                            </Select>
-                        </FormControl>
-                        <br />
-                        <TextField label="Username" value={username} margin='normal' onChange={handleChange('username')} required/>
-                        <br />
-                        <FormControl fullWidth="true">
-                            <InputLabel shrink htmlFor="unit-simple">Employee Name</InputLabel>
-                            <Select
-                                value={mEmployeeId}
-                                onChange={handleChange('mEmployeeId')}
-                                inputProps={{
-                                    name: 'mEmployeeId',
-                                    id: 'unit-simple',
-                                }}
-                            >
-                                <MenuItem valu="">
-                                    <em>-Select Role Name-</em>
-                                    </MenuItem>
-                                {employees.map(emp => {
-                                    return (
-                                        <MenuItem value={emp._id}>{emp.first_name + ' ' + emp.last_name}</MenuItem>
-                                    )
-                                })}
-
-                            </Select>
-                        </FormControl>
-                        <br />
-                        <TextField label="Password" value={password} margin='normal' type="password" onChange={handleChange('password')} required/>
-                        <br />
-                        <TextField label="Re-Type Password" value={re_password} margin='normal' type="password" onChange={handleChange('re_password')} required />
-                        <br />
-                    </form>
-                </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose} color="primary">
-                    Cancel
-            </Button>
-                <Button onClick={handleSubmit} color="primary" autoFocus>
-                    Save
-            </Button>
-            </DialogActions>
-        </Dialog>
+    <Button onClick={handleToggle} variant="contained" color="primary" style={{ float:'right' }}>Add</Button>
+    <Dialog open={createNew} onClose={handleClose} >
+    <DialogContent>
+    <DialogContentText id="alert-dialog-description">
+    <div class="title">Add Marketing Promotion</div>
+    <form class="martop">
+    <FormControl fullWidth='true'>
+    <InputLabel shrink htmlFor="menu-simple" >* Select Event :</InputLabel>
+    <Select
+    value={t_event_id}
+    onChange={handleChange('t_event_id')}
+    inputProps={{
+        name: 't_event_id',
+        id: 'menu-simple'
+    }}
+    displayEmpty
+    >
+    <MenuItem value='' style={{color:'grey', fontFamily:'Helvetica'}}>
+    -Select Event-
+    </MenuItem>
+    {events.map(x => {
+        return(
+            <MenuItem key={x._id} value={x._id}>{x.code}</MenuItem>
+        )
+    })}
+    </Select>
+    </FormControl>
+    
+    </form>
+    </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+    <Button onClick={handleClose} color="secondary" variant="contained">
+    Cancel
+    </Button>
+    <Button onClick={handleSubmit} color="primary" variant="contained" autoFocus>
+    Save
+    </Button>
+    </DialogActions>
+    </Dialog>
     </Fragment>
 }
--- src/components/promotion/index.js
+++ src/components/promotion/index.js
@@ -4,255 +4,261 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import CreateUser from './create';
-import EditUser from './edit';
-import DeleteUser from './delete';
-import { Button } from '../../../node_modules/@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import SearchIcon from '@material-ui/icons/Search'
+import IconButton from '@material-ui/core/IconButton';
+import CreatePromo from './create';
+// import EditPromo from './edit';
+// import DeletePromo from './delete';
+// import ViewPromo from './view';
+import { config } from '../configuration/config';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import axios from 'axios';
-import { config } from '../configuration/config';
-class Users extends React.Component {
 
-    userModel = {
-        _id: '', username: '', password: '', re_password: '', m_role_id: '', m_employee_id: '',
-        mRoleCode: '', mRoleName: '', mRoleDescription: '', mEmployeeFirstName: '', mEmployeeLastName: '', mEmployeeEmail: '',
-        mEmployeemCompanyId: '', mEmployeemCompanyName: ''
-    };
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: [],
-            roles: [],
-            employees: [],
-            createNew: false,
-            editUser: false,
-            selectData: 'user-aggregation',
-            load: true,
-            user: this.userModel
-        }
+class Promotions extends React.Component {
+  
+  promoModel = {
+    _id: '',
+    code:'',
+    flag_design:'',
+    title:'',
+    t_event_id: '',
+    is_delete: '',
+    note:'',
+  }
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      promos: [],
+      employes:[],
+      events:[],
+      createNew: false,
+      deletePromo:false,
+      editPromo:false,
+      loading: true,
+      promo:this.promoModel
     }
+  }
+  
+  reloadEmployeeData = () => {
+    axios.get(config.url + '/m_employee_role')
+    .then(res =>{
+      this.setState({
+        employes:res.data
+      })
+    })
+    .catch((error)=> {
+      alert(error);
+    })
+  }
 
-    reloadEmployeData = () => {
-        axios.get(config.url + '/m-employee')
-            .then(res => {
-                this.setState({
-                    employees: res.data
-                })
-            })
-            .catch((error) => {
-                alert(error);
-            })
+  reloadEventData = () => {
+    axios.get(config.url + '/t-event')
+    .then(res =>{
+      this.setState({
+        events:res.data
+      })
+    })
+    .catch((error)=> {
+      alert(error);
+    })
+  }
+  reloadPromoData = () => {
+    axios.get(config.url + '/t_promotion_ctrl')
+    .then(res => {
+      this.setState({
+        promos: res.data,
+        createNew: false,
+        deletePromo:false,
+        editPromo:false,
+        promo: this.promoModel,
+        loading:false
+      })
+    })
+    .catch((error)=> {
+      alert(error);
+    })
+  }
+  
+  componentDidMount(){
+    this.reloadPromoData();
+    this.reloadEmployeeData();
+    this.reloadEventData();
+  }
+  
+  handleToggle = () => {
+    this.setState({
+      createNew: true,
+      promo: this.promoModel
+    })
+  }
+  
+  handleClose = () => {
+    this.setState({
+      createNew:false,
+      deletePromo:false,
+      editPromo:false,
+      viewPromo:false,
+      promo: this.promoModel
+    })
+  }
+  
+  handleChange = name => ({ target: { value } }) => {
+    this.setState({
+      promo: {
+        ...this.state.promo,
+        [name]: value
+      }
+    })
+  }
+  
+  handleSubmit = () => {
+    const { promo, createNew } = this.state;
+    let newPromo = {
+      code: promo.code,
     }
-
-    reloadRoleData = () => {
-        axios.get(config.url + '/m-role')
-            .then(res => {
-                this.setState({
-                    roles: res.data
-                })
-            })
-            .catch((error) => {
-                alert(error);
-            })
+    
+    if (createNew) {
+      axios.post(config.url + '/t-promotion', newPromo)
+      .then(res => {
+        this.reloadPromoData();
+        alert('Data Saved! New Promo has been added');
+      })
+      .catch((error) => {
+        alert(error)
+      })
+    }else{
+      axios.put(config.url + '/t-promotion/' + promo._id, newPromo)
+      .then(res => {
+        this.reloadPromoData();
+        alert('Data Updated! New promo has been updated');
+      })
+      .catch((error) => {
+        alert(error)
+      })
     }
-    componentDidMount() {
-        this.reloadUserData();
-        this.reloadEmployeData();
-        this.reloadRoleData();
-    }
+  }
 
-    reloadUserData = () => {
-        axios.get(config.url + '/user-aggregation')
-            .then(res => {
-                this.setState({
-                    users: res.data,
-                    createNew: false,
-                    editUser: false,
-                    deleteUser: false,
-                    user: this.userModel,
-                    load: false
-                })
-            })
-            .catch((error) => {
-                alert(error);
-            })
-    }
+  handleEdit = (_id) => {
+    const { promos } = this.state;
+    const promo = promos.find(m => m._id === _id);
+    this.setState({
+      editPromo: true,
+      promo:{
+        _id: promo._id,
+        code: promo.code,
+      }
+    })
+  }
 
-    handleToggle = () => {
-        this.setState({
-            createNew: true,
-            updateUser: true,
-            user: this.userModel
-        });
-    }
+  handleDelete = (_id) => {
+    const { promos } = this.state;
+    const promo = promos.find(m => m._id === _id);
+    this.setState({
+      deletePromo: true,
+      promo:{
+        _id: promo._id,
+        code: promo.code,
+      }
+    })
+  }
 
+  handleView = (_id) => {
+    const { promos } = this.state;
+    const promo = promos.find(m => m._id === _id);
+    this.setState({
+      viewPromo: true,
+      promo:{
+        _id: promo._id,
+        code: promo.code,
+      }
+    })
+  }
 
-
-    handleClose = () => {
-        this.setState({
-            createNew: false,
-            editUser: false,
-            deleteUser: false
-        });
-    }
-
-    handleChange = name => ({ target: { value } }) => {
-        this.setState({
-            user: {
-                ...this.state.user,
-                [name]: value
-            }
+  handleDeleteConfirm = () => {
+    const { promo } = this.state;
+    console.log(promo._id);
+    axios.delete(config.url + '/t-promotion/' + promo._id)
+        .then(res => {
+            this.reloadPromoData();
+            alert('Data Deleted! Promo has been deleted. \n');
         })
-    };
-
-    handleChangeCheckBox = name => event => {
-        this.setState({
-            user: {
-                ...this.state.user,
-                [name]: event.target.checked
-            }
-        });
-    }
-
-    handleEdit = (n) => {
-        const { users } = this.state;
-        const user = users.find(u => u._id === n);
-        this.setState({
-            editUser: true,
-            user: {
-                _id: user._id,
-                username: user.username,
-                password: user.password,
-                mRoleId: user.m_role_id,
-                mRoleName: user.mRoleName,
-                mEmployeeId: user.m_employee_id,
-                mEmployeeFirstName: user.mEmployeeFirstName,
-                mEmployeeLastName: user.mEmployeeLastName,
-                re_password: user.password
-            }
-        });
-    }
-
-    handleSubmit = () => {
-        const { user, createNew } = this.state;
-
-        let newUser = {
-            username: user.username,
-            password: user.password,
-            m_role_id: user.mRoleId,
-            m_employee_id: user.mEmployeeId,
-        }
-
-        if (createNew) {
-            axios.post(config.url + '/m-user', newUser)
-                .then(res => {
-                    this.reloadUserData();
-                    alert('User has been saved.\n');
-                })
-                .catch((error) => {
-                    alert(error);
-                })
-        } else {
-            axios.put(config.url + '/m-user/' + user._id, newUser)
-                .then(res => {
-                    this.reloadUserData();
-                });
-        }
-    }
-
-    handleDelete = (n) => {
-        const { users } = this.state;
-        console.log(users);
-        const user = users.find(u => u._id === n);
-        // console.log(user);
-        this.setState({
-            deleteUser: true,
-            user: {
-                _id: user._id,
-                username: user.username,
-                password: user.password,
-                mEmployeeFirstName: user.mEmployeeFirstName,
-                mEmployeeLastName: user.mEmployeeLastName,
-                mRoleName: user.mRoleName,
-            }
-        });
-    }
-
-    handleDeleteConfirm = () => {
-        const { user } = this.state;
-        axios.delete(config.url + '/m-user/' + user._id)
-            .then(res => {
-                this.reloadUserData();
-                alert('Data Berhasil Dihapus!')
-            })
-            .catch((error) => {
-                alert('Error');
-            })
-    }
-
-    render() {
-        const { users, load, employeess } = this.state;
-        const { classes } = this.props;
-        let i = 1;
+        .catch((error) => {
+            alert(error)
+        })
+  }
+  
+  render(){
+    const { promos, loading, events, employes } = this.state;
+    const { classes } = this.props;
+    let i=1;
+    return(
+      <div>
+      <h3 style={{color:'#3f51b5'}}><center>List Promotions</center></h3>
+      <CreatePromo createNew={this.state.createNew} handleToggle={this.handleToggle} handleClose={this.handleClose} handleChange={this.handleChange} handleSubmit={this.handleSubmit} promo={this.state.promo} events={this.state.events} />
+      {/* <DeletePromo deletePromo={this.state.deletePromo} handleClose={this.handleClose} handleDelete={this.handleDeleteConfirm} promo={this.state.promo}/>
+      <ViewPromo viewPromo={this.state.viewPromo} handleClose={this.handleClose} promo={this.state.promo}/>
+      <EditPromo editPromo={this.state.editPromo} handleToggle={this.handleToggle} handleClose={this.handleClose} handleChange={this.handleChange} handleSubmit={this.handleSubmit} promo={this.state.promo} /> */}
+      <Table>
+      <TableHead>
+      <TableRow>
+      <TableCell style={{fontWeight:"bold", color:"black"}}>No</TableCell>
+      <TableCell style={{fontWeight:"bold", color:"black"}}>Transaction Code</TableCell>
+      <TableCell style={{fontWeight:"bold", color:"black"}}>Request By</TableCell>
+      <TableCell style={{fontWeight:"bold", color:"black"}}>Request Date</TableCell>
+      <TableCell style={{fontWeight:"bold", color:"black"}}>Assign To</TableCell>
+      <TableCell style={{fontWeight:"bold", color:"black"}}>Status</TableCell>
+      <TableCell style={{fontWeight:"bold", color:"black"}}>Created Date</TableCell>
+      <TableCell style={{fontWeight:"bold", color:"black"}}>Create By</TableCell>
+      <TableCell style={{textAlign:"center",fontWeight:"bold", color:"black"}}>is_delete</TableCell>
+      <TableCell style={{textAlign:"center",fontWeight:"bold", color:"black"}}>Action</TableCell>
+      </TableRow>
+      </TableHead>
+      <TableBody>
+      {promos.map(n => {
         return (
-            <div>
-                <h3></h3>
-                <CreateUser createNew={this.state.createNew} handleToggle={this.handleToggle} handleClose={this.handleClose} handleChange={this.handleChange} handleSubmit={this.handleSubmit} handleChangeCheckBox={this.handleChangeCheckBox} user={this.state.user} employees={this.state.employees} roles={this.state.roles} />
-                <EditUser editUser={this.state.editUser} handleToggle={this.handleToggle} handleChangeCheckBox={this.handleChangeCheckBox} handleClose={this.handleClose} handleChange={this.handleChange} handleSubmit={this.handleSubmit} user={this.state.user} employees={this.state.employees} roles={this.state.roles} />
-                <DeleteUser deleteUser={this.state.deleteUser} handleClose={this.handleClose} handleDelete={this.handleDeleteConfirm} user={this.state.user} />
-                <CircularProgress className={classes.progress} style={{ visibility: (load ? 'visible' : 'hidden') }} color="secondary" />
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>No</TableCell>
-                            <TableCell>Employee</TableCell>
-                            <TableCell>Role</TableCell>
-                            <TableCell>Company</TableCell>
-                            <TableCell>Username</TableCell>
-                            <TableCell>Created Data</TableCell>
-                            <TableCell>Create By</TableCell>
-                            <TableCell>Action</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {users.map(n => {
-                            return (
-                                <TableRow key={n._id}>
-                                    <TableCell>{i++}</TableCell>
-                                    <TableCell>{n.mEmployeeFirstName + ' ' + n.mEmployeeLastName}</TableCell>
-                                    <TableCell component="th" scope="row">{n.mRoleName}</TableCell>
-                                    <TableCell>{n.mEmployeemCompanyName}</TableCell>
-                                    <TableCell>{n.username}</TableCell>
-                                    <TableCell>{n.createDate}</TableCell>
-                                    <TableCell>{n.mRoleName}</TableCell>
-                                    <TableCell>
-                                        <Button onClick={() => this.handleEdit(n._id)} variant="contained" color="primary">Edit</Button>
-                                        <Button onClick={() => this.handleDelete(n._id)} variant="contained" color="secondary">Delete</Button>
-                                    </TableCell>
-                                </TableRow>
-                            );
-                        })}
-                    </TableBody>
-                </Table>
-            </div>
-        )
-    }
+          <TableRow key={n._id}>
+          <TableCell>{i++}</TableCell>
+          <TableCell component="th" scope="row">
+          {n.code}
+          </TableCell>
+          <TableCell>{(n.request_by.first ? n.request_by.first + " " : "")+(n.request_by.last ? n.request_by.last + " " : "")}</TableCell>
+          <TableCell>{n.request_date}</TableCell>
+          <TableCell></TableCell>
+          <TableCell>{n.status}</TableCell>
+          <TableCell>{n.createDate}</TableCell>
+          <TableCell>{(n.created_by.first ? n.created_by.first + " " : "")+(n.created_by.last ? n.created_by.last + " " : "")}</TableCell>
+          <TableCell style={{textAlign:"center"}}>{n.is_delete}</TableCell>
+          <TableCell style={{textAlign:"center"}}>
+          <IconButton onClick={() => this.handleView(n._id)}><SearchIcon color="primary" /></IconButton>
+          <IconButton onClick={() => this.handleEdit(n._id)}><EditIcon color="primary" /></IconButton>
+          <IconButton onClick={() => this.handleDelete(n._id)}><DeleteIcon color="secondary" /></IconButton> 
+          </TableCell>
+          </TableRow>
+        );
+      })}
+      </TableBody>
+      </Table>
+      </div>
+    )
+  }
 }
 
 const styles = theme => ({
-    progress: {
-        position: 'absolute',
-        alignSelf: 'center',
-        top: '50%',
-        left: '50%',
-        alignItem: 'center'
-    },
+  progress: {
+    position: 'absolute',
+    alignSlef: 'center',
+    top: '50%',
+    alignItem: 'center'
+  },
 });
 
-Users.propTypes = {
-    classes: PropTypes.object.isRequired,
+Promotions.PropTypes = {
+  classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Users);
+export default withStyles(styles)(Promotions);
