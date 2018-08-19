d9bd99df4d506e2e2fe22217ec78c8968c347f39
WildanBalfas <wildanbalfas3@gmail.com>
2018-08-20 04:28:22 +0700

    update promotion

--- src/components/promotion/create.js
+++ src/components/promotion/create.js
@@ -9,17 +9,17 @@ import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 
-export default ({createNew,handleToggle,handleChange,handleClose,handleSubmit, promo: {t_event_id, event_code}, events}) =>{
-    
+export default ({createNew,handleToggle,handleChange,handleClose,handleSubmit, promo: {t_event_id, flag_design, t_design_id}, events, designs}) =>{
     return <Fragment>
     <Button onClick={handleToggle} variant="contained" color="primary" style={{ float:'right' }}>Add</Button>
-    <Dialog open={createNew} onClose={handleClose} >
-    <DialogContent>
+    <Dialog open={createNew} onClose={handleClose}  >
+    <DialogContent style={{paddimngLeft:25, paddingRight:25}}>
     <DialogContentText id="alert-dialog-description">
     <div class="title">Add Marketing Promotion</div>
     <form class="martop">
+    <br/>
     <FormControl fullWidth='true'>
-    <InputLabel shrink htmlFor="menu-simple" >* Select Event :</InputLabel>
+    <InputLabel shrink htmlFor="menu-simple" >* Select Event</InputLabel>
     <Select
     value={t_event_id}
     onChange={handleChange('t_event_id')}
@@ -39,7 +39,49 @@ export default ({createNew,handleToggle,handleChange,handleClose,handleSubmit, p
     })}
     </Select>
     </FormControl>
-    
+    <br/>
+    <br/>
+    <FormControl fullWidth='true'>
+    <InputLabel shrink htmlFor="menu-simple">* Select from Design</InputLabel>
+    <Select
+    value={flag_design}
+    onChange={handleChange('flag_design')}
+    inputProps={{
+        name: 'flag_design',
+        id: 'menu-simple'
+    }}
+    displayEmpty
+    >
+    <MenuItem value='' style={{color:'grey', fontFamily:'Helvetica'}}>
+    -Please Select-
+    </MenuItem>
+    <MenuItem value='yes' >Yes</MenuItem>
+    <MenuItem value='no'>No</MenuItem>
+    </Select>
+    </FormControl>
+    <br/>
+    <br/>
+    <FormControl fullWidth='true'>
+    <InputLabel shrink htmlFor="menu-simple" >* Select Design</InputLabel>
+    <Select
+    value={t_design_id}
+    onChange={handleChange('t_design_id')}
+    inputProps={{
+        name: 't_design_id',
+        id: 'menu-simple'
+    }}
+    displayEmpty
+    >
+    <MenuItem value='' style={{color:'grey', fontFamily:'Helvetica'}}>
+    -Select Design-
+    </MenuItem>
+    {designs.map(z => {
+        return(
+            <MenuItem key={z._id} value={z._id}>{z.code}</MenuItem>
+        )
+    })}
+    </Select>
+    </FormControl>
     </form>
     </DialogContentText>
     </DialogContent>
--- src/components/promotion/index.js
+++ src/components/promotion/index.js
@@ -19,94 +19,115 @@ import { withStyles } from '@material-ui/core/styles';
 import axios from 'axios';
 
 class Promotions extends React.Component {
-  
+
   promoModel = {
     _id: '',
-    code:'',
-    flag_design:'',
-    title:'',
+    code: '',
+    flag_design: '',
+    title: '',
     t_event_id: '',
+    t_design_id: '',
     is_delete: '',
-    note:'',
+    note: '',
   }
-  
-  constructor(props){
+
+  constructor(props) {
     super(props);
     this.state = {
       promos: [],
-      employes:[],
-      events:[],
+      employes: [],
+      events: [],
+      designs: [],
       createNew: false,
-      deletePromo:false,
-      editPromo:false,
+      deletePromo: false,
+      editPromo: false,
+      subSelect: false,
       loading: true,
-      promo:this.promoModel
+      promo: this.promoModel
     }
   }
-  
+
   reloadEmployeeData = () => {
     axios.get(config.url + '/m_employee_role')
-    .then(res =>{
-      this.setState({
-        employes:res.data
+      .then(res => {
+        this.setState({
+          employes: res.data,
+          loading: false,
+        })
       })
-    })
-    .catch((error)=> {
-      alert(error);
-    })
+      .catch((error) => {
+        alert(error);
+      })
   }
 
   reloadEventData = () => {
     axios.get(config.url + '/t-event')
-    .then(res =>{
-      this.setState({
-        events:res.data
+      .then(res => {
+        this.setState({
+          events: res.data,
+          loading: false,
+        })
       })
-    })
-    .catch((error)=> {
-      alert(error);
-    })
+      .catch((error) => {
+        alert(error);
+      })
   }
+
+  reloadDesignData = () => {
+    axios.get(config.url + '/t-design')
+      .then(res => {
+        this.setState({
+          designs: res.data,
+          loading: false,
+        })
+      })
+      .catch((error) => {
+        alert(error);
+      })
+  }
+
   reloadPromoData = () => {
     axios.get(config.url + '/t_promotion_ctrl')
-    .then(res => {
-      this.setState({
-        promos: res.data,
-        createNew: false,
-        deletePromo:false,
-        editPromo:false,
-        promo: this.promoModel,
-        loading:false
+      .then(res => {
+        this.setState({
+          promos: res.data,
+          createNew: false,
+          deletePromo: false,
+          editPromo: false,
+          promo: this.promoModel,
+          loading: false
+        })
       })
-    })
-    .catch((error)=> {
-      alert(error);
-    })
+      .catch((error) => {
+        alert(error);
+      })
   }
-  
-  componentDidMount(){
+
+  componentDidMount() {
     this.reloadPromoData();
     this.reloadEmployeeData();
     this.reloadEventData();
+    this.reloadDesignData();
   }
-  
+
   handleToggle = () => {
     this.setState({
       createNew: true,
+      subSelect: false,
       promo: this.promoModel
     })
   }
-  
+
   handleClose = () => {
     this.setState({
-      createNew:false,
-      deletePromo:false,
-      editPromo:false,
-      viewPromo:false,
+      createNew: false,
+      deletePromo: false,
+      editPromo: false,
+      viewPromo: false,
       promo: this.promoModel
     })
   }
-  
+
   handleChange = name => ({ target: { value } }) => {
     this.setState({
       promo: {
@@ -115,31 +136,31 @@ class Promotions extends React.Component {
       }
     })
   }
-  
+
   handleSubmit = () => {
     const { promo, createNew } = this.state;
     let newPromo = {
       code: promo.code,
     }
-    
+
     if (createNew) {
       axios.post(config.url + '/t-promotion', newPromo)
-      .then(res => {
-        this.reloadPromoData();
-        alert('Data Saved! New Promo has been added');
-      })
-      .catch((error) => {
-        alert(error)
-      })
-    }else{
+        .then(res => {
+          this.reloadPromoData();
+          alert('Data Saved! New Promo has been added');
+        })
+        .catch((error) => {
+          alert(error)
+        })
+    } else {
       axios.put(config.url + '/t-promotion/' + promo._id, newPromo)
-      .then(res => {
-        this.reloadPromoData();
-        alert('Data Updated! New promo has been updated');
-      })
-      .catch((error) => {
-        alert(error)
-      })
+        .then(res => {
+          this.reloadPromoData();
+          alert('Data Updated! New promo has been updated');
+        })
+        .catch((error) => {
+          alert(error)
+        })
     }
   }
 
@@ -148,7 +169,7 @@ class Promotions extends React.Component {
     const promo = promos.find(m => m._id === _id);
     this.setState({
       editPromo: true,
-      promo:{
+      promo: {
         _id: promo._id,
         code: promo.code,
       }
@@ -160,7 +181,7 @@ class Promotions extends React.Component {
     const promo = promos.find(m => m._id === _id);
     this.setState({
       deletePromo: true,
-      promo:{
+      promo: {
         _id: promo._id,
         code: promo.code,
       }
@@ -172,7 +193,7 @@ class Promotions extends React.Component {
     const promo = promos.find(m => m._id === _id);
     this.setState({
       viewPromo: true,
-      promo:{
+      promo: {
         _id: promo._id,
         code: promo.code,
       }
@@ -183,66 +204,67 @@ class Promotions extends React.Component {
     const { promo } = this.state;
     console.log(promo._id);
     axios.delete(config.url + '/t-promotion/' + promo._id)
-        .then(res => {
-            this.reloadPromoData();
-            alert('Data Deleted! Promo has been deleted. \n');
-        })
-        .catch((error) => {
-            alert(error)
-        })
+      .then(res => {
+        this.reloadPromoData();
+        alert('Data Deleted! Promo has been deleted. \n');
+      })
+      .catch((error) => {
+        alert(error)
+      })
   }
-  
-  render(){
-    const { promos, loading, events, employes } = this.state;
+
+  render() {
+    const { promos, loading, events, employes, designs } = this.state;
     const { classes } = this.props;
-    let i=1;
-    return(
+    let i = 1;
+    return (
       <div>
-      <h3 style={{color:'#3f51b5'}}><center>List Promotions</center></h3>
-      <CreatePromo createNew={this.state.createNew} handleToggle={this.handleToggle} handleClose={this.handleClose} handleChange={this.handleChange} handleSubmit={this.handleSubmit} promo={this.state.promo} events={this.state.events} />
-      {/* <DeletePromo deletePromo={this.state.deletePromo} handleClose={this.handleClose} handleDelete={this.handleDeleteConfirm} promo={this.state.promo}/>
+        <h3 style={{ color: '#3f51b5' }}><center>List Promotions</center></h3>
+        <CircularProgress className={classes.progress} style={{ visibility: (loading ? 'visible' : 'hidden') }} color="secondary" />
+        <CreatePromo createNew={this.state.createNew} handleToggle={this.handleToggle} handleClose={this.handleClose} handleChange={this.handleChange} handleSubmit={this.handleSubmit} promo={this.state.promo} events={this.state.events} designs={this.state.designs} />
+        {/* <DeletePromo deletePromo={this.state.deletePromo} handleClose={this.handleClose} handleDelete={this.handleDeleteConfirm} promo={this.state.promo}/>
       <ViewPromo viewPromo={this.state.viewPromo} handleClose={this.handleClose} promo={this.state.promo}/>
       <EditPromo editPromo={this.state.editPromo} handleToggle={this.handleToggle} handleClose={this.handleClose} handleChange={this.handleChange} handleSubmit={this.handleSubmit} promo={this.state.promo} /> */}
-      <Table>
-      <TableHead>
-      <TableRow>
-      <TableCell style={{fontWeight:"bold", color:"black"}}>No</TableCell>
-      <TableCell style={{fontWeight:"bold", color:"black"}}>Transaction Code</TableCell>
-      <TableCell style={{fontWeight:"bold", color:"black"}}>Request By</TableCell>
-      <TableCell style={{fontWeight:"bold", color:"black"}}>Request Date</TableCell>
-      <TableCell style={{fontWeight:"bold", color:"black"}}>Assign To</TableCell>
-      <TableCell style={{fontWeight:"bold", color:"black"}}>Status</TableCell>
-      <TableCell style={{fontWeight:"bold", color:"black"}}>Created Date</TableCell>
-      <TableCell style={{fontWeight:"bold", color:"black"}}>Create By</TableCell>
-      <TableCell style={{textAlign:"center",fontWeight:"bold", color:"black"}}>is_delete</TableCell>
-      <TableCell style={{textAlign:"center",fontWeight:"bold", color:"black"}}>Action</TableCell>
-      </TableRow>
-      </TableHead>
-      <TableBody>
-      {promos.map(n => {
-        return (
-          <TableRow key={n._id}>
-          <TableCell>{i++}</TableCell>
-          <TableCell component="th" scope="row">
-          {n.code}
-          </TableCell>
-          <TableCell>{(n.request_by.first ? n.request_by.first + " " : "")+(n.request_by.last ? n.request_by.last + " " : "")}</TableCell>
-          <TableCell>{n.request_date}</TableCell>
-          <TableCell></TableCell>
-          <TableCell>{n.status}</TableCell>
-          <TableCell>{n.createDate}</TableCell>
-          <TableCell>{(n.created_by.first ? n.created_by.first + " " : "")+(n.created_by.last ? n.created_by.last + " " : "")}</TableCell>
-          <TableCell style={{textAlign:"center"}}>{n.is_delete}</TableCell>
-          <TableCell style={{textAlign:"center"}}>
-          <IconButton onClick={() => this.handleView(n._id)}><SearchIcon color="primary" /></IconButton>
-          <IconButton onClick={() => this.handleEdit(n._id)}><EditIcon color="primary" /></IconButton>
-          <IconButton onClick={() => this.handleDelete(n._id)}><DeleteIcon color="secondary" /></IconButton> 
-          </TableCell>
-          </TableRow>
-        );
-      })}
-      </TableBody>
-      </Table>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell style={{ fontWeight: "bold", color: "black" }}>No</TableCell>
+              <TableCell style={{ fontWeight: "bold", color: "black" }}>Transaction Code</TableCell>
+              <TableCell style={{ fontWeight: "bold", color: "black" }}>Request By</TableCell>
+              <TableCell style={{ fontWeight: "bold", color: "black" }}>Request Date</TableCell>
+              <TableCell style={{ fontWeight: "bold", color: "black" }}>Assign To</TableCell>
+              <TableCell style={{ fontWeight: "bold", color: "black" }}>Status</TableCell>
+              <TableCell style={{ fontWeight: "bold", color: "black" }}>Created Date</TableCell>
+              <TableCell style={{ fontWeight: "bold", color: "black" }}>Create By</TableCell>
+              <TableCell style={{ textAlign: "center", fontWeight: "bold", color: "black" }}>is_delete</TableCell>
+              <TableCell style={{ textAlign: "center", fontWeight: "bold", color: "black" }}>Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {promos.map(n => {
+              return (
+                <TableRow key={n._id}>
+                  <TableCell>{i++}</TableCell>
+                  <TableCell component="th" scope="row">
+                    {n.code}
+                  </TableCell>
+                  <TableCell>{(n.request_by.first ? n.request_by.first + " " : "") + (n.request_by.last ? n.request_by.last + " " : "")}</TableCell>
+                  <TableCell>{n.request_date}</TableCell>
+                  <TableCell></TableCell>
+                  <TableCell>{n.status}</TableCell>
+                  <TableCell>{n.createDate}</TableCell>
+                  <TableCell>{(n.created_by.first ? n.created_by.first + " " : "") + (n.created_by.last ? n.created_by.last + " " : "")}</TableCell>
+                  <TableCell style={{ textAlign: "center" }}>{n.is_delete}</TableCell>
+                  <TableCell style={{ textAlign: "center" }}>
+                    <IconButton onClick={() => this.handleView(n._id)}><SearchIcon color="primary" /></IconButton>
+                    <IconButton onClick={() => this.handleEdit(n._id)}><EditIcon color="primary" /></IconButton>
+                    <IconButton onClick={() => this.handleDelete(n._id)}><DeleteIcon color="secondary" /></IconButton>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
       </div>
     )
   }
